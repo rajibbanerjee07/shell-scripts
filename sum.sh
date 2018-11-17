59bd4d6ba8e909518ebdb3de166192fc7623d825
root <root@ip-172-31-47-220.us-east-2.compute.internal>
2018-11-17 10:41:30 +0000

    initial shell scripts

---
+++ sum.sh
@@ -0,0 +1,9 @@
+sum=0
+for var in "$@"
+ do
+  if [ $var -gt 10 ]
+   then
+    sum=`expr $sum + $var`
+  fi
+ done
+echo $sum
---
+++ test.sh
@@ -0,0 +1,6 @@
+echo "Hello World"
+x=10
+echo $x
+echo $#
+sum=`expr $1 + $2 + $3`
+echo $sum 
